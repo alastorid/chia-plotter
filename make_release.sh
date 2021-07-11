229302cfdeb2835f540ed9e924ed124e0a21c099
alastorid <alastorid@gmail.com>
2021-07-12 01:51:28 +0800

    Update make_release.sh
    it is faster

--- make_release.sh
+++ make_release.sh
@@ -4,7 +4,5 @@ mkdir -p build
 
 cd build
 
-cmake -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false ..
-
-make -j8 $@
-
+cmake -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1 -funroll-loops -fomit-frame-pointer -finline-small-functions -march=native -mtune=native" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false ..
+make -j $@
